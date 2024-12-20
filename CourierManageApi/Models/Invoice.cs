5dad0ce5386afecb1b4c226e886df266e6c87abc
Arafat <arafat.developer2008@gmail.com>
2024-12-20 09:06:12 +0600

    Add project files.

---
+++ CourierManageApi/Models/Invoice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourierManageApi.Models;
+
+public partial class Invoice
+{
+    public int InvoiceId { get; set; }
+
+    public DateTime PaymentTime { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string? Particular { get; set; }
+
+    public string CreateBy { get; set; } = null!;
+
+    public DateTime CreateDate { get; set; }
+
+    public string? UpdateBy { get; set; }
+
+    public string? UpdateDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public int PaymentMethodId { get; set; }
+
+    public int ParcelsId { get; set; }
+
+    public virtual Customer Customer { get; set; } = null!;
+
+    public virtual Parcel Parcels { get; set; } = null!;
+
+    public virtual PaymentMethod PaymentMethod { get; set; } = null!;
+}
---
+++ CourierManageApi/Models/Parcel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourierManageApi.Models;
+
+public partial class Parcel
+{
+    public int ParcelId { get; set; }
+
+    public string ParcelCode { get; set; } = null!;
+
+    public int SenderCustomerId { get; set; }
+
+    public int ReceiverCustomerId { get; set; }
+
+    public DateTime? SendTime { get; set; }
+
+    public DateTime? ReceiveTime { get; set; }
+
+    public int SenderBranchId { get; set; }
+
+    public int ReceiverBranchId { get; set; }
+
+    public DateTime EstimatedReceiveTime { get; set; }
+
+    public bool IsPaid { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string CreateBy { get; set; } = null!;
+
+    public DateTime? CreateDate { get; set; }
+
+    public string UpdateBy { get; set; } = null!;
+
+    public DateTime? UpdateDate { get; set; }
+
+    public bool IsDispatchedFromBranch { get; set; }
+
+    public bool IsInTransit { get; set; }
+
+    public bool IsReceivedAtBranch { get; set; }
+
+    public bool IsReceivedByReceiver { get; set; }
+
+    public int? VanId { get; set; }
+
+    public int? DriverId { get; set; }
+
+    public int? DeliveryChargeId { get; set; }
+
+    public int? ParcelTypeId { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual DeliveryCharge? DeliveryCharge { get; set; }
+
+    public virtual ICollection<Invoice> Invoices { get; set; } = new List<Invoice>();
+
+    public virtual ParcelType? ParcelType { get; set; }
+
+    public virtual Branch ReceiverBranch { get; set; } = null!;
+
+    public virtual Branch SenderBranch { get; set; } = null!;
+
+    public virtual Van? Van { get; set; }
+}
